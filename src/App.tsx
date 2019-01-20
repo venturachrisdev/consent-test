7ccf44558906b29e29446fb2b3ad3c23142ba706
Christopher Ventura <cventura@hurrabit.com>
2019-01-20 10:04:48 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+// Screens
+import Home from './screens/Home';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <>
+        <Home/>
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ src/screens/Home/Home.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+export default class Home extends React.PureComponent {
+
+  render() {
+    return (
+      <p>Hello Home</p>
+    );
+  }
+}
